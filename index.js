16cc85cac590e9be5092ccfec1d8876c7a0553aa
Rovshan Badirkhanov <rovsanbadirxanov@gmail.com>
2020-03-16 02:57:47 +0400

    Second commit

---
+++ index.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+	Array.from(document.getElementsByClassName('boli')).forEach(
+		(img) => {
+			let
+				div = document.createElement('div'),
+				img_bg = img.cloneNode(true);
+			img.replaceWith(div),
+			div.appendChild(img),
+			img_bg.style.filter = "blur(8px)",
+			div.appendChild(img_bg),
+			img.style.position = 'absolute',
+			img.style.zIndex = '1'
+		})
+}
