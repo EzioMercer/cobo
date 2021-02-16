2291c7f5dac4d999b744c41278b53544c20ca8fb
EzioMercer <rovsanbadirxanov@gmail.com>
2021-02-17 02:54:26 +0400

    change variables type

--- cobo.js
+++ cobo.js
@@ -3,27 +3,21 @@ export default ({hoverMode = false} = {}) => {
 	if(typeof hoverMode !== 'boolean') throw new TypeError('hoverMode must be boolean type');
 	if(event.target.tagName !== 'IMG') throw new Error('Element is not IMG');
 
-	var
-		img = event.target,
-		div = document.createElement('div'),
-		img_bg = img.cloneNode(true);
+	const img = event.target;
+	const div = document.createElement('div');
+	const img_bg = img.cloneNode(true);
 
 	img.replaceWith(div);
 	div.appendChild(img);
 
 	if(hoverMode){
-		
 		img.onmouseover = () => img_bg.style.filter = 'blur(8px)';
 		img.onmouseout = () => img_bg.style.filter = 'blur(0px)';
-
 	} else {
-		
 		img_bg.style.filter = 'blur(8px)';
-		
 	}
 
 	div.appendChild(img_bg);
 	img.style.position = 'absolute';
 	img.style.zIndex = '1';
-		
 };
